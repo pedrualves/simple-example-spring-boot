4465562b56df952aad300521c93aaa8be75b544e
Alves <pedro.alves@accenture.com>
2017-12-27 11:13:30 -0200

    first!

---
+++ src/main/java/teste/testando/Application.java
@@ -0,0 +1,14 @@
+package teste.testando;
+
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+		new SpringApplicationBuilder().bannerMode(Mode.OFF).sources(Application.class).run(args);
+	}
+
+}
---
+++ src/main/java/teste/testando/controller/BeanController.java
@@ -0,0 +1,40 @@
+package teste.testando.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import teste.testando.bean.Bean;
+
+@RestController
+public class BeanController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+	Bean home() {
+		Bean a = new Bean();
+		a.setAbc("123");
+		a.setNum(321);
+		return a;
+	}
+
+	@PostMapping("/teste/post")
+	Bean testePost(@RequestBody Bean bean) {
+		return bean;
+	}
+
+	@GetMapping("/teste/get")
+	String testeGet(@RequestParam(required = false) String param) {
+		return param;
+	}
+
+	@GetMapping("/teste/outro/{param}/get")
+	String testeGet2(@PathVariable String param) {
+		return param;
+	}
+
+}
